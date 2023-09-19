cb7cc8ac94d6802cec308f2c75187b94d5fbcfda
Heriberto Martinez <heriberto.martinez@sngular.com>
2023-09-18 18:58:55 -0600

    Fancy comments for analize script are added

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -5,15 +5,15 @@ n=1
 while read line; do
 
     if [[ $line == *"Estimated power use"* ]]; then
-        echo "Line No. $n : $line"
+        echo -e "\e[34mLine No. $n : $line\e[0m"
     fi
 
     if [[ $line == *"Total cpu time"* ]]; then
-        echo "Line No. $n : $line"
+        echo -e "\e[34mLine No. $n : $line\e[0m"
     fi
 
     if [[ $line == *"Proc"* ]]; then
-        echo "Line No. $n : $line"
+        echo -e "\e[34mLine No. $n : $line\e[0m"
     fi
 
     n=$((n+1))
