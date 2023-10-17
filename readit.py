544567f8407bb6b934759779cfa88594c75024bf
Omar Marín <82827012+devom7022-sngular@users.noreply.github.com>
2023-10-17 15:04:11 -0600

    adjust virtual log to algorithm

--- readit.py
+++ readit.py
@@ -11,20 +11,6 @@ doc= open("results.txt","w+")
 fileName= 'batterylogs.txt'
 package= "com.example.batterytestapplication"
 
-def manageBankData(item): 
-    for node in item:
-        sectionated = re.split('=',node)
-        #print("sectionated",sectionated)
-        itemName = sectionated[0]
-        itemValue = sectionated[1]
-        
-        if not itemName in uIdNamesList:
-            uIdNamesList.append(itemName)
-            uIdValuesList.append(itemValue)
-        else: 
-            pos = uIdNamesList.index(itemName)
-            uIdValuesList[pos] = float(uIdValuesList[pos]) + float(itemValue)
-    
 def checkIsTypePhysical():
     with open(fileName) as file:
         while True:
@@ -68,34 +54,24 @@ with open(fileName) as file:
             capData = re.split(",|:",line)
             lineCap1 = "Capacidad de batería "+ capData[1].strip().lstrip()+ " mAh\r\n"
             lineCap2 = "Batería drenada por procesos "+ capData[3].strip().lstrip()+ " mAh\r\n\r\n"
-            
             doc.writelines([lineCap1,lineCap2])
-       
-        if isPhysical == False: 
-            arr =  re.split(':|[(]|[)]',line)
-            uIdTotal +=  float(arr[1])
-            paramsId = re.split(" ",arr[2].strip())
-            for stack in paramsId:
-                manageBankData(re.split(',',stack))
-        else:
-            if "UID "+appID in line:
-                #Primero separar los valores con mayor jerarquía 
-                segments = re.split('[(]|[)]',line)
-                line = line.strip()
-                indexChilds = line.index('(')
-                mainData = re.split(' ',line[0:indexChilds].strip())
-                #add params and values to array
-                for i in range(1,len(mainData)):
-                    if ":" in mainData[i] :
-                        uIdNamesList.append(mainData[i])
-                        
-                    else:
-                        uIdValuesList.append(mainData[i])
-                
-                #manage data child
+
+        if "uid "+appID in line.lower():
+            line = line.strip()
+            indexChilds = line.index('(')
+            mainData = re.split(' ',line[0:indexChilds].strip())
+            #add params and values to array
+            for i in range(1,len(mainData)):
+                if ":" in mainData[i] :
+                    uIdNamesList.append(mainData[i])
+                    
+                else:
+                    uIdValuesList.append(mainData[i])
+            
+            #manage data child
+            if isPhysical == True:
                 childData = line[indexChilds+1: len(line)-1 ]
                 cildrenArr = re.split('=',childData)
-                
                 uIdNamesList.append(cildrenArr[0].strip())
 
                 for i in range(1,len(cildrenArr)-1):
@@ -106,41 +82,31 @@ with open(fileName) as file:
                     firstName = segment[0:indexSegment]
                     uIdValuesList.append(firstName)
                     uIdNamesList.append(nextValue)
-                    print(nextValue,indexSegment, firstName)
-
                     
                 uIdValuesList.append(cildrenArr[len(cildrenArr)-1])
-                
+            else:
+                cildrenArr = re.split('[(]|[)]',line)                    
 
+                for i in range(1,len(cildrenArr)-1):
+                    segment= cildrenArr[i].strip()
+                    print(segment)
+                    if '=' in segment:
+                        cutSegment= re.split(' |=',segment)
+                    elif ':' in segment:
+                        cutSegment= re.split(':',segment)
+                    
+                    print("seg",cutSegment)
+                    for i in range(0,len(cutSegment)):
+                        modulo = i % 2
+                        print("modulo",modulo)
+                        if modulo == 0:
+                            print("con 0",cutSegment[i])
+                            uIdNamesList.append(cutSegment[i])
+                        else:
+                            print("con 1",cutSegment[i])
+                            uIdValuesList.append(cutSegment[i])
 
-                # childData = childData.replace(":",">").replace(")","),").replace("=",":=").replace(" (","(")
-                # childData = re.split(',',childData)
-
-                # for i in range(0,len(childData)-1):
-                #     values = childData[i].count(':=') 
-                #     if values > 1:
-                #         nesteds = re.split(" ",childData[i])
-                #         childData.pop(i)
-                #         for j in range(len(nesteds)):
-                #             childData.append(nesteds[j])
-
-                # for child in childData:
-                #     child = re.split('=',child.strip())
-
-                #     if len(child) == 1:
-                #         continue
-
-                #     for i in range(0,len(child)):
-                #         if ":" in child[i] :
-                #             uIdNamesList.append(child[i].strip())
-                #         else:
-                #             uIdValuesList.append(child[i].strip())
-            
 #Empieza a escribir .txt
-#if(isPhysical == False):
-#uIdTotalStr = "Uso total de energía = ",str(uIdTotal)+ " mAh\r\n"
-#doc.writelines(uIdTotalStr)
-
 tableNames = []
 
 i = 0
